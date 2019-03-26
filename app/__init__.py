0546f96dafdcd55323290209569ada094c67a827
Tegar Imansyah <tegar@alterra.id>
2019-03-26 13:50:30 +0700

    implement delay and response

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, request
-from app.api_handling import api_handling
 import configparser
+from time import sleep
+
+from app.api_handling import api_handling
+
+app = Flask(__name__)
 
 cfg = configparser.ConfigParser()
 cfg.read('config.cfg')
-
-app = Flask(__name__)
 data = api_handling(cfg['file']['mock_test'])
 
 @app.route('/health')
@@ -15,9 +17,15 @@ def health_checker():
 @app.route('/', defaults={'path': ''}, methods=['GET','POST','UPDATE','PUT'])
 @app.route('/<path:path>', methods=['GET','POST','UPDATE','PUT'])
 def catch_all(path):
-    response = data.path.get('/' + path,{}).get(request.method, False)
+    result = data.path.get('/' + path,{}).get(request.method, False)
     
-    if response is False or response is None:
+    if result is False or result is None:
         return jsonify({'status':'Error','msg':'Not Found'}), 404
+    
+    response = result.get('response',{})
+    wait = int(result.get('wait',0))
+
+    if wait > 0:
+        sleep(wait/1000)
 
     return jsonify(response), 200
