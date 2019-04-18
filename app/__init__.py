b5a11cf5c472697be498be3141d26de28cc46c50
Tegar Imansyah <tegar@alterra.id>
2019-04-18 16:38:34 +0700

    main app

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,35 @@
-from flask import Flask, jsonify, request
-import configparser
-from time import sleep
+# import configparser
+# from time import sleep
 
-from app.api_handling import api_handling
+from flask import Flask
+
+# from app.api_handling import api_handling
+from app.membership.web.urls import membership
 
 app = Flask(__name__)
 
-cfg = configparser.ConfigParser()
-cfg.read('config.cfg')
-data = api_handling(cfg['file']['mock_test'])
+app.register_blueprint(membership, url_prefix='/membership')
 
-@app.route('/health')
-def health_checker():
-    return ('', 204)
+# cfg = configparser.ConfigParser()
+# cfg.read('config.cfg')
+# data = api_handling(cfg['file']['mock_test'])
 
-@app.route('/', defaults={'path': ''}, methods=['GET','POST','UPDATE','PUT'])
-@app.route('/<path:path>', methods=['GET','POST','UPDATE','PUT'])
-def catch_all(path):
-    result = data.path.get('/' + path,{}).get(request.method, False)
+# @app.route('/health')
+# def health_checker():
+#     return ('', 204)
+
+# @app.route('/', defaults={'path': ''}, methods=['GET','POST','UPDATE','PUT'])
+# @app.route('/<path:path>', methods=['GET','POST','UPDATE','PUT'])
+# def catch_all(path):
+#     result = data.path.get('/' + path,{}).get(request.method, False)
     
-    if result is False or result is None:
-        return jsonify({'status':'Error','msg':'Not Found'}), 404
+#     if result is False or result is None:
+#         return jsonify({'status':'Error','msg':'Not Found'}), 404
     
-    response = result.get('response',{})
-    wait = int(result.get('wait',0))
+#     response = result.get('response',{})
+#     wait = int(result.get('wait',0))
 
-    if wait > 0:
-        sleep(wait/1000)
+#     if wait > 0:
+#         sleep(wait/1000)
 
-    return jsonify(response), 200
+#     return jsonify(response), 200
